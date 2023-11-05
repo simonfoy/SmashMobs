409c6142c85e84e821162c999458edb24a7e21dd
simonfoy <simonfoy1@proton.me>
2023-11-05 17:53:44 -0500

    Initial commit

---
+++ src/main/java/me/simonfoy/smashmobs/SmashMobs.java
@@ -0,0 +1,17 @@
+package me.simonfoy.smashmobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmashMobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
